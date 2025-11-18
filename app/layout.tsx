f74d76912dc5e9c350e71e42077f5dc710d5d142
Bhone Khant <bhonekhant@agga.io>
2025-11-18 15:06:13 +0630

    base

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 // Style တွေလည်း ဒီမှာ import လုပ်နိုင်ပါတယ် (ဥပမာ- Tailwind CSS)
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="my">
       <body>
